653ebc84ceea132881e938d1699acfa1069324ff
Michie740 <myang740@gmail.com>
2020-11-21 16:17:15 -0500

    removed ; and added input/output files

--- Board.py
+++ Board.py
@@ -1,33 +1,31 @@
 class Board:
 	# initilizing board with domains
 	def __init__(self, fn):
-		default = [1,2,3,4,5,6,7,8,9];
-		self.board = [[default[:] for j in range(9)] for i in range (9)];
+		default = [1,2,3,4,5,6,7,8,9]
+		self.board = [[default[:] for j in range(9)] for i in range (9)]
 
-		self.readFile(fn);
+		self.readFile(fn)
 
 
 	# reading file and restricing domains based on the initial file
 	def readFile(self, fn):
-		f = open(fn, 'r');
-
-		r = 0;
+		f = open(fn, 'r')
+		r = 0
 		while r < 9:
-			line = f.readline().strip().split();
-			c = 0;
+			line = f.readline().strip().split()
+			c = 0
 			while c < 9:
-				val = int(line[c]);
+				val = int(line[c])
 				if val != 0:
-					self.board[r][c] = [val];
-					self.constrainRows(r, c, val);
-					self.constrainCols(r, c, val);
-					self.constrainBox(r, c, val);
+					self.board[r][c] = [val]
+					self.constrainRows(r, c, val)
+					self.constrainCols(r, c, val)
+					self.constrainBox(r, c, val)
 					if not (((r % 4) == 0) or ((c % 4) == 0)):
-						self.constrainHyper(r, c, val);
-				c += 1;
-			r += 1;
-
-		f.close();
+						self.constrainHyper(r, c, val)
+				c += 1
+			r += 1
+		f.close()
 		return
 
 
@@ -35,30 +33,30 @@ class Board:
 	def constrainRows(self, r, c, val):
 		for i in range(9):
 			if (val in self.board[r][i]) and (i != c):
-				self.board[r][i].remove(val);
+				self.board[r][i].remove(val)
 
 		# constrain domain based on columns
 	def constrainCols(self, r, c, val):
 		for i in range(9):
 			if val in self.board[i][c] and (i!= r):
-				self.board[i][c].remove(val);
+				self.board[i][c].remove(val)
 
 
 		# constrain domain based on boxes
 	def constrainBox(self, r, c, val):
-		cQ = c // 3;
-		rQ = r // 3;
-		i = 0;
+		cQ = c // 3
+		rQ = r // 3
+		i = 0
 		while i < 3:
-			j = 0;
+			j = 0
 			while j < 3:
-				queryR = (3 * rQ) + i;
-				queryC = (3 * cQ) + j;
+				queryR = (3 * rQ) + i
+				queryC = (3 * cQ) + j
 				# if val is in box remove from domain
 				if val in self.board[queryR][queryC] and not((queryR == r) and (queryC == c)):
-					self.board[queryR][queryC].remove(val);
-				j += 1;
-			i += 1;
+					self.board[queryR][queryC].remove(val)
+				j += 1
+			i += 1
 
 
 		# contrain domain based on hyperboxes
@@ -95,6 +93,6 @@ class Board:
 		out = ''
 		for row in self.board:
 			for col in row:
-				out += str(col) + ' ';
-			out += '\n\n';
-		return out;
+				out += str(col) + ' '
+			out += '\n\n'
+		return out
